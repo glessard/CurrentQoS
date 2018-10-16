12af1b55da0bd6afd60360d3768eb59614f29fc5
Guillaume Lessard <dhtnstff@gmail.com>
2018-10-16 16:41:42 -0600

    add a manually-built Xcode project

---
+++ Xcode/CurrentQoS/CurrentQoS.h
@@ -0,0 +1,19 @@
+//
+//  CurrentQoS.h
+//  CurrentQoS
+//
+//  Created by Guillaume Lessard on 10/16/18.
+//  Copyright © 2018 Guillaume Lessard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CurrentQoS.
+FOUNDATION_EXPORT double CurrentQoSVersionNumber;
+
+//! Project version string for CurrentQoS.
+FOUNDATION_EXPORT const unsigned char CurrentQoSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CurrentQoS/PublicHeader.h>
+
+
