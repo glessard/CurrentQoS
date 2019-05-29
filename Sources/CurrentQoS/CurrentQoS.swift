2db22b5eb98b3a050ab624690a77b2ecc4bc0ea9
Guillaume Lessard <dhtnstff@gmail.com>
2019-05-28 19:00:59 -0600

    require compiler 4.2 and up

--- Sources/CurrentQoS/CurrentQoS.swift
+++ Sources/CurrentQoS/CurrentQoS.swift
@@ -6,28 +6,16 @@ import Dispatch
 
 extension DispatchQoS
 {
-#if swift(>=4.1.50)
   @inlinable
   static public var current: DispatchQoS {
     guard let qosClass = DispatchQoS.QoSClass.current
       else { return .default }
     return DispatchQoS(qosClass: qosClass, relativePriority: 0)
   }
-#else
-  static public var current: DispatchQoS {
-    @inline(__always)
-    get {
-      guard let qosClass = DispatchQoS.QoSClass.current
-        else { return .default }
-      return DispatchQoS(qosClass: qosClass, relativePriority: 0)
-    }
-  }
-#endif
 }
 
 extension DispatchQoS.QoSClass
 {
-#if swift(>=4.1.50)
   @inlinable
   static public var current: DispatchQoS.QoSClass? {
 #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
@@ -36,16 +24,4 @@ extension DispatchQoS.QoSClass
     return nil
 #endif
   }
-#else
-  static public var current: DispatchQoS.QoSClass? {
-    @inline(__always)
-    get {
-#if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
-      return DispatchQoS.QoSClass(rawValue: qos_class_self())
-#else // platforms that rely on swift-corelibs-libdispatch
-      return nil
-#endif
-    }
-  }
-#endif
 }
